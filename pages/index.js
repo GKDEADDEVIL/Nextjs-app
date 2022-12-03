2daffcbaf57af32a9fc3e52e4e987b56d03f2481
GKDAREDEVIL <kalyangopalam@gmail.com>
2022-12-03 22:17:05 +0530

    hero

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+    <Header />
       <Hero />
     </div>
   )
